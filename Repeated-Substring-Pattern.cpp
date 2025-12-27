0dd951659c8f13341393827444f43641f2be68b3
Ishita Singh <24cs2019@rgipt.ac.in>
2025-12-27 14:09:41 +0530

    [459] [Time Beats: 87.09%] [Memory Beats: 76.39%] - LeetPush

--- Repeated-Substring-Pattern.cpp
+++ Repeated-Substring-Pattern.cpp
@@ -1,21 +1,8 @@
 1class Solution {
 2public:
 3    bool repeatedSubstringPattern(string s) {
-4    int n = s.size();
-5
-6    for (int len = 1; len <= n / 2; len++) {
-7        if (n % len != 0) continue;
-8
-9        string pattern = s.substr(0, len);
-10        string built = "";
-11
-12        for (int i = 0; i < n / len; i++) {
-13            built += pattern;
-14        }
-15
-16        if (built == s) return true;
-17    }
-18
-19    return false;
-20}
-21};
+4    string t = s + s;
+5    return t.substr(1, t.size() - 2).find(s) != string::npos;
+6}
+7
+8};
