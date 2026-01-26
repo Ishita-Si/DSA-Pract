93ee217e549c39590c23b201a78894c103219642
Ishita Singh <24cs2019@rgipt.ac.in>
2026-01-26 18:20:53 +0530

    [441] [Time Beats: 14.70%] [Memory Beats: 68.71%] - LeetPush

--- Arranging-Coins.cpp
+++ Arranging-Coins.cpp
@@ -1,12 +1,10 @@
 1class Solution {
 2public:
 3    int arrangeCoins(int n) {
-4        long long count = 0;
-5        long long a = 0;
-6      while(n>=a*(a+1)/2){
-7        count++;
-8        a++;
-9      }
-10      return count-1;  
-11    }
-12};
+4        long long a = 0;
+5      while(n>=a*(a+1)/2){
+6        a++;
+7      }
+8      return a-1;  
+9    }
+10};
