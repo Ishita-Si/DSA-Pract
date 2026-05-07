aa949a25584ff945268e54ab142c3ceed56f92b8
Ishita Singh <24cs2019@rgipt.ac.in>
2026-05-07 13:33:30 +0530

    [137] [Time Beats: 100.00%] [Memory Beats: 63.98%] - LeetPush

--- Single-Number-II.cpp
+++ Single-Number-II.cpp
@@ -1,13 +1,14 @@
 1class Solution {
 2public:
 3    int singleNumber(vector<int>& nums) {
-4        int n = nums.size();
-5        sort(nums.begin(),nums.end());
-6        for(int i = 1; i < n; i = i+3){
-7            if(nums[i-1] != nums[i]) return nums[i-1];
-8            if( nums[i+1] != nums[i] ) return nums[i+1];
-9        }
-10
-11        return nums[n-1];
-12    }
-13};
+4        int ones = 0; 
+5        int twos = 0;
+6
+7        for(int i = 0; i < nums.size(); i++){
+8            ones = (ones^nums[i]) & ~ twos;
+9            twos = (twos^nums[i]) & ~ ones;
+10        }
+11
+12        return ones;
+13    }
+14};
