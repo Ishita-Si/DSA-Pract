917088ee331125ae8052546a4a6e6deb963bbef9
Ishita Singh <24cs2019@rgipt.ac.in>
2026-05-09 22:32:27 +0530

    [904] [Time Beats: 7.92%] [Memory Beats: 8.31%] - LeetPush

--- Fruit-Into-Baskets.cpp
+++ Fruit-Into-Baskets.cpp
@@ -4,23 +4,22 @@
 4        int n = fruits.size();
 5        int maxLen = 0;
 6        int l = 0, r = 0;
-7        
-8        map<int,int> maps;
+7
+8        map<int, int> maps;
 9
-10        while(r < n){
+10        while (r < n) {
 11            maps[fruits[r]]++;
-12            if(maps.size() > 2){
-13                while(maps.size() > 2){
-14                    maps[fruits[l]]--;
-15                    if(maps[fruits[l]] == 0) maps.erase(fruits[l]);
-16                    l++;
-17                }
-18            }
-19            if(maps.size() <= 2){
-20                maxLen = max(maxLen , r-l+1);
-21            }
-22            r++;
-23        }
-24        return maxLen;
-25    }
-26};
+12            if (maps.size() > 2) {
+13                maps[fruits[l]]--;
+14                if (maps[fruits[l]] == 0)
+15                    maps.erase(fruits[l]);
+16                l++;
+17            }
+18            if (maps.size() <= 2) {
+19                maxLen = max(maxLen, r - l + 1);
+20            }
+21            r++;
+22        }
+23        return maxLen;
+24    }
+25};
