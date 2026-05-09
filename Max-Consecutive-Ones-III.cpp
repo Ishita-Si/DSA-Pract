a698e2dd9562cdce5d916de7c22f81edc880a75e
Ishita Singh <24cs2019@rgipt.ac.in>
2026-05-09 16:27:15 +0530

    [1004] [Time Beats: 100.00%] [Memory Beats: 92.23%] - LeetPush

--- Max-Consecutive-Ones-III.cpp
+++ Max-Consecutive-Ones-III.cpp
@@ -2,25 +2,24 @@
 2public:
 3    int longestOnes(vector<int>& nums, int k) {
 4        int l = 0, r = 0;
-5        int maxLen = 0, len = 0;
+5        int len = 0;
 6        int zeros = 0;
-7
-8        while (r < nums.size()) {
-9            if (nums[r] == 0) {
-10                zeros++;
-11            }
-12            while (zeros > k) {
-13                if (nums[l] == 0) {
-14                    zeros--;
-15                }
-16                l++; 
-17            }
-18            if (zeros <= k) {
-19                    len = r - l + 1;
-20                    maxLen = max(len, maxLen);
-21                }
-22                r++;
-23        }
-24        return maxLen;
-25    }
-26};
+7        int maxLen = 0;
+8        int n = nums.size();
+9
+10        while(r < n){
+11            if(nums[r] == 0) zeros++;
+12            if(zeros > k){
+13                if(nums[l] == 0) zeros--;
+14                l++;
+15            }
+16            if(zeros <= k){
+17                len = r - l + 1;
+18                maxLen = max(len, maxLen);
+19            } 
+20            r++;
+21        }
+22
+23        return maxLen;
+24    }
+25};
