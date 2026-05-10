8b9f51696c2e61c6552cf15e6d346a959d83f6f4
Ishita Singh <24cs2019@rgipt.ac.in>
2026-05-10 23:30:38 +0530

    [229] [Time Beats: 100.00%] [Memory Beats: 96.56%] - LeetPush

--- Majority-Element-II.cpp
+++ Majority-Element-II.cpp
@@ -1,16 +1,40 @@
 1class Solution {
 2public:
 3    vector<int> majorityElement(vector<int>& nums) {
-4        vector<int> ans;
-5        map<int, int> mp;
-6        for (int x : nums) {
-7            mp[x]++;
-8        }
-9        for (auto it : mp) {
-10            if (it.second > (nums.size() / 3)) {
-11                ans.push_back(it.first);
+4        int cnt1 = 0, cnt2 = 0;
+5        int el1 = INT_MIN, el2 = INT_MIN;
+6
+7        for(int x : nums){
+8
+9            if(cnt1 == 0 && x != el2){
+10                el1 = x;
+11                cnt1 = 1;
 12            }
-13        }
-14        return ans;
-15    }
-16};
+13            else if(cnt2 == 0 && x != el1){
+14                el2 = x;
+15                cnt2 = 1;
+16            }
+17            else if(x == el1) cnt1++;
+18            else if(x == el2) cnt2++;
+19            else{
+20                cnt1--;
+21                cnt2--;
+22            }
+23        }
+24
+25        cnt1 = 0;
+26        cnt2 = 0;
+27
+28        for(int x : nums){//real freq
+29            if(x == el1) cnt1++;
+30            else if(x == el2) cnt2++;
+31        }
+32
+33        vector<int> ans;
+34
+35        if(cnt1 > nums.size()/3) ans.push_back(el1);
+36        if(cnt2 > nums.size()/3) ans.push_back(el2);
+37
+38        return ans;
+39    }
+40};
