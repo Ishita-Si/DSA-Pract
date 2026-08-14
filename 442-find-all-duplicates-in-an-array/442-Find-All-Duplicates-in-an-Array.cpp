84207609e4c41476a3849a49e634734618015d04
Ishita Singh <24cs2019@rgipt.ac.in>
2026-08-14 11:06:43 +0530

    [442] [Time Beats: 53.55%] [Memory Beats: 82.23%] - LeetPush

--- 442-find-all-duplicates-in-an-array/442-Find-All-Duplicates-in-an-Array.cpp
+++ 442-find-all-duplicates-in-an-array/442-Find-All-Duplicates-in-an-Array.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-        unordered_map<int,int> freq;
         vector<int> ans;
-
-        for(int i = 0; i < nums.size(); ++i){
-            freq[nums[i]]++;
-            if(freq[nums[i]] == 2) ans.push_back(nums[i]);
-        } 
-
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            int index = abs(nums[i]) - 1;
+            if (nums[index] < 0) {
+                ans.push_back(index + 1);
+            } else {
+                nums[index] = -nums[index];
+            }
+        }
+        
         return ans;
     }
 };
