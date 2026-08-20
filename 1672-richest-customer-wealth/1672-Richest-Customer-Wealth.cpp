e273cde10538c604a2b68a32b75f1b66c9008fbb
Ishita Singh <24cs2019@rgipt.ac.in>
2026-08-20 09:52:39 +0530

    [1672] [Time Beats: 100.00%] [Memory Beats: 55.73%] - LeetPush

---
+++ 1672-richest-customer-wealth/1672-Richest-Customer-Wealth.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int n = accounts.size();
+        int m = accounts[0].size();
+        int maxW = 0;
+        for(int i = 0; i < n; i++){
+            int sum = 0;
+            for(int j = 0; j < m;j++){
+                sum += accounts[i][j];
+            }
+            maxW = max(maxW, sum);
+        }
+
+        return maxW;
+    }
+};
